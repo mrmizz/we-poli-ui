e75fdd83b373d7ce204c90485d74ed7d8ddcd2d5
Alexander Binaei <alex.binaei@carpe.io>
2020-08-18 19:30:26 -0700

    Code TLC.

--- src/Main.elm
+++ src/Main.elm
@@ -420,7 +420,7 @@ viewSearchConfirmed model =
 viewVertexNamePrefixResponse : Model -> Html Msg
 viewVertexNamePrefixResponse model =
     ul [ class "dropdown" ]
-        ([ text "Potential Search Matches:" ] ++ (buildPotentialSearchMatch model.vertex_data_response))
+        ([ text "Potential Search Matches:" ] ++ buildPotentialSearchMatch model.vertex_data_response)
 
 
 buildPotentialSearchMatch : List VertexData -> List (Html Msg)
@@ -615,16 +615,22 @@ fromTitleToUrlHtml : String -> Html Msg
 fromTitleToUrlHtml title =
     li [] [ a [ Html.Attributes.target "_blank", Html.Attributes.href (fromTitleToUrl title) ] [ text title ] ]
 
+
+
 -- TODO: more fields
+
+
 fromVertexDataToHTML : VertexData -> Html Msg
 fromVertexDataToHTML vertexData =
-    li [] [
-        ul [] [
-            li [] [ text "name:"
-                , ul [] [ li [] [text vertexData.name] ]
-            ],
-            li [] [ text "is_comittee:"
-                , ul [] [ li [] [text (printBool vertexData.is_committee)] ]
+    li []
+        [ ul []
+            [ li []
+                [ text "name:"
+                , ul [] [ li [] [ text vertexData.name ] ]
+                ]
+            , li []
+                [ text "is_comittee:"
+                , ul [] [ li [] [ text (printBool vertexData.is_committee) ] ]
+                ]
             ]
         ]
-    ]
