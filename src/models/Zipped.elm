40495f41c9b6212f793a586d1f42e92a3ee63f0a
Alexander Binaei <alex.binaei@carpe.io>
2020-09-17 18:42:09 -0700

    TODO: move zipped methods to Models.

--- src/models/Zipped.elm
+++ src/models/Zipped.elm
@@ -3,6 +3,6 @@ module Models.Zipped exposing (..)
 import Models.EdgeData exposing (EdgeData)
 import Models.VertexData exposing (VertexData)
 
-
+-- TODO: move methods here
 type alias Zipped =
     ( EdgeData, VertexData )
