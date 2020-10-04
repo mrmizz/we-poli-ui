5990fc1fb028219573353953134c9f98a6c7f4b1
Alexander Binaei <alex.binaei@carpe.io>
2020-10-04 12:02:48 -0700

    more test coverage for zip method.

--- tests/models/zipped/ZipVerticesAndEdgesSpec.elm
+++ tests/models/zipped/ZipVerticesAndEdgesSpec.elm
@@ -10,14 +10,14 @@ import Test exposing (Test, describe, test)
 suite1 : Test
 suite1 =
     describe "Zip Vertices and Edges"
-        [ describe "should handle multiple traversals from different src ids"
-            [ test "direction in" <|
+        [ describe "should handle a single traversal"
+            [ test "that is pre-sorted" <|
                 \_ ->
                     let
                         actual : List Zipped
                         actual =
                             zipVerticesAndEdges
-                                In
+                                Out
                                 [ traversal1 ]
                                 [ vendor1, vendor2, vendor3 ]
                                 [ edge1, edge2, edge3 ]
@@ -25,5 +25,49 @@ suite1 =
                     Expect.equal
                         actual
                         zipped1
+            , test "that is not pre-sorted" <|
+                \_ ->
+                    let
+                        actual : List Zipped
+                        actual =
+                            zipVerticesAndEdges
+                                Out
+                                [ traversal1 ]
+                                [ vendor1, vendor3, vendor2 ]
+                                [ edge3, edge2, edge1 ]
+                    in
+                    Expect.equal
+                        actual
+                        zipped1
+            ]
+        , describe "should handle multiple traversals from different src ids"
+            [ test "that is pre-sorted" <|
+                \_ ->
+                    let
+                        actual : List Zipped
+                        actual =
+                            zipVerticesAndEdges
+                                Out
+                                [ traversal1, traversal2 ]
+                                [ vendor1, vendor2, vendor3, vendor4 ]
+                                [ edge1, edge2, edge3, edge4, edge5, edge6 ]
+                    in
+                    Expect.equal
+                        actual
+                        zipped2
+            , test "that is not pre-sorted" <|
+                \_ ->
+                    let
+                        actual : List Zipped
+                        actual =
+                            zipVerticesAndEdges
+                                Out
+                                [ traversal1, traversal2 ]
+                                [ vendor1, vendor3, vendor2, vendor4 ]
+                                [ edge6, edge2, edge4, edge3, edge5, edge1 ]
+                    in
+                    Expect.equal
+                        actual
+                        zipped2
             ]
         ]
