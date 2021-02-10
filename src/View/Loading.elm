1f11db95ecd8b0bfe2c41880aae9b39be938d2d6
Alexander Binaei <alex.binaei@carpe.io>
2021-02-09 18:29:47 -0800

    Loading page, with animation

--- src/View/Loading.elm
+++ src/View/Loading.elm
@@ -1,13 +1,23 @@
 module View.Loading exposing (view)
 
 import Html exposing (Html)
+import Html.Attributes exposing (class)
 import Msg.Msg exposing (Msg)
-import View.Header
+import View.Hero
 
 
 view : Html Msg
 view =
+    View.Hero.view body
+
+
+body : Html Msg
+body =
     Html.div
-        []
-        [ View.Header.view
+        [ class "container"
+        ]
+        [ Html.div
+            [ class "has-loading has-text-centered"
+            ]
+            []
         ]
